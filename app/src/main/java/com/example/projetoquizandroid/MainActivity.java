fffb59b8b3666a3b37f393102aba16554f1c7b82
Arthur-Ferreira-v <marketingthur15@gmail.com>
2025-09-23 11:05:04 -0300

    colors - strings

--- app/src/main/java/com/example/projetoquizandroid/MainActivity.java
+++ app/src/main/java/com/example/projetoquizandroid/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.projetoquizandroid;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+    TextView txvPergunta;
+
+    RadioGroup rdgPrincipal;
+
+    Button btnResponder;
+
+    int pontuacao = 0;
+
+    int indicePergunta = 0;
+
+    String []
 }
